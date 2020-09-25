d6a9a0a1038fe969a8a1b70159aa58a964ed1e90
Swetha Janardhan <swethaiyer26@gmail.com>
2020-09-25 16:08:24 +0530

    Add files via upload

---
+++ Node_express_v1.js
@@ -0,0 +1,19 @@
+var express=require('express');
+var app=express();
+app.use(express.static('public'));
+app.get('/process.html',function(req,res){
+    res.sendFile(__dirname + "/" + "process.html");
+})
+app.get('/process_get',function(req,res){
+    response={
+        first_name:req.query.first_name,
+        last_name:req.query.last_name
+    };
+    console.log(response);
+    res.end(JSON.stringify());
+})
+var server=app.listen(8081,function(){
+    var host=server.address().address
+    var port=server.address().port
+    console.log("Example app listening at http://%s:%s", host, port)
+})
